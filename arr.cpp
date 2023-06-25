0443d26ac0901a03b455f27824f06dd55549cfab
Erikpiliposyan <135508562+Erikpiliposyan@users.noreply.github.com>
2023-06-25 12:17:08 +0400

    Add files via upload

---
+++ arr.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Array{
+
+public:
+    Array(int* ptr , int size)
+        :ptr(ptr)
+        ,size(size){}
+    
+    ~Array(){
+
+        delete[] ptr;
+    }
+    
+    Array(const Array& other)
+        :size(other.size)
+    {
+        ptr = new int [size];
+        for(int i = 0 ; i < size ; i++){
+            ptr[i] = other.ptr[i];
+        }
+    }
+
+    Array& operator=(const Array& other){
+        if(this == &other){
+            return *this;
+        }
+
+        delete[] ptr;
+        ptr = new int [other.size];
+        size = other.size;
+        for(int i = 0 ; i < size; i++)
+        {
+            ptr[i] = other.ptr[i];
+        }
+
+        return *this;
+    }
+    int gen_element(int);
+private:
+    int* ptr;
+    int size;
+};
