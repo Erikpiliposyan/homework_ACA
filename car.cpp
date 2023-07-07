2b952cdb61f8be09432b5fe2dcb397176ceb276e
Erikpiliposyan <135508562+Erikpiliposyan@users.noreply.github.com>
2023-07-07 11:31:16 +0400

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -3,17 +3,49 @@
 
 class Car{
     virtual void type() const = 0;
+
+    
 };
 
 class Mycar: public Car{
     public:
-        Mycar(const std::string& brand , const std::string& model)
-            :brand(brand)
-            ,model(model)
-        {}
-        void type()const  override{
-            std::cout << "Type the " << brand << " , " << model << " car." << std::endl;
+        Mycar(const std::strin  enum class Color {
+        red,
+        green,
+        purple,
+        orange,
+        blue,
+        white,
+        yellow,
+        black
+    };
+
+    static std::string colorToString(Color color) {
+        switch (color) {
+            case Color::red:
+                return "Red";
+            case Color::green:
+                return "Green";
+            case Color::purple:
+                return "Purple";
+            case Color::orange:
+                return "Orange";
+            case Color::blue:
+                return "Blue";
+            case Color::white:
+                return "White";
+            case Color::yellow:
+                return "Yellow";
+            case Color::black:
+                return "Black";
+            default:
+                return "Unknown";
         }
+    }
+};
+    void type()const  override{
+        std::cout << "Type the " << brand << " , " << model << " car." << std::endl;
+    }
 
     private:
         std::string brand;
